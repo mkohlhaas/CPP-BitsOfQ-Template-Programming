c405f007eb1016518fc4e455d1a8aaa249affeba
Hans-J. Schmid <knock@myopendoor.de>
2025-06-25 00:31:43 +0200

    E07

--- Metaprogramming/E07_compile_time_meets_run_time/main.cpp
+++ Metaprogramming/E07_compile_time_meets_run_time/main.cpp
@@ -6,42 +6,53 @@ main()
 {
     using namespace bits_of_q;
 
-    auto tup = make_tuple(8, true, 1.5);
-    std::cout << get<2>(tup) << '\n';            // 1.5
+    {
+        auto tup = make_tuple(8, true, 1.5);
+        std::cout << get<2>(tup) << '\n';            // 1.5
 
-    const Tuple const_tup{1, false, 4};
-    std::cout << get<2>(const_tup) << '\n';      // 4
+        const Tuple const_tup{1, false, 4};
+        std::cout << get<2>(const_tup) << '\n';      // 4
 
-    std::cout << get<2>(Tuple{2, 3, 8}) << '\n'; // 8
-    get<2>(tup) = 4.2;
-    std::cout << get<2>(tup) << '\n';            // 4.2
+        std::cout << get<2>(Tuple{2, 3, 8}) << '\n'; // 8
+        get<2>(tup) = 4.2;
+        std::cout << get<2>(tup) << '\n';            // 4.2
+    }
 
-    CopyCounter c;
-    CopyCounter::reset();
+    {
+        CopyCounter c;
+        CopyCounter::reset();
 
-    make_tuple(8, c, 1.5);
-    CopyStats our_stats = c.stats;
+        make_tuple(8, c, 1.5);
+        CopyStats our_stats = c.stats;
+        CopyCounter::reset();
 
-    CopyCounter::reset();
-    std::make_tuple(8, c, 1.5);
-    CopyStats std_stats = c.stats;
+        std::make_tuple(8, c, 1.5);
+        CopyStats std_stats = c.stats;
 
-    std::cout << "bits_of_q::make_tuple: " << our_stats << '\n';
-    std::cout << "std::make_tuple:       " << std_stats << '\n';
+        std::cout << "bits_of_q::make_tuple: " << our_stats << '\n';
+        std::cout << "std::make_tuple:       " << std_stats << '\n';
 
-    assert(our_stats == std_stats);
+        assert(our_stats == std_stats);
+    }
 
-    CopyCounter::reset();
-    get<1>(tup);
-    our_stats = CopyCounter::stats;
+    {
+        auto tup = make_tuple(8, true, 1.5);
 
-    auto std_tup = std::make_tuple(8, c, 1.5);
-    CopyCounter::reset();
-    get<1>(std_tup);
-    std_stats = CopyCounter::stats;
+        CopyCounter c;
+        CopyCounter::reset();
 
-    std::cout << "bits_of_q::get:        " << our_stats << '\n';
-    std::cout << "std::get:              " << std_stats << '\n';
+        get<1>(tup);
+        CopyStats our_stats = CopyCounter::stats;
 
-    assert(our_stats == std_stats);
+        auto std_tup = std::make_tuple(8, c, 1.5);
+        CopyCounter::reset();
+
+        get<1>(std_tup);
+        CopyStats std_stats = CopyCounter::stats;
+
+        std::cout << "bits_of_q::get:        " << our_stats << '\n';
+        std::cout << "std::get:              " << std_stats << '\n';
+
+        assert(our_stats == std_stats);
+    }
 }
