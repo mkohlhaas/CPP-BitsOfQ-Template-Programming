9c4e36e0a461d11d488dc6fbb7de80f8185e5a70
Hans-J. Schmid <knock@myopendoor.de>
2025-06-25 22:28:46 +0200

    cleanup E01

--- Metaprogramming/E01_intro_to_template_metaprogramming/main.cpp
+++ Metaprogramming/E01_intro_to_template_metaprogramming/main.cpp
@@ -3,20 +3,30 @@
 
 // is_pointer
 
-template <typename T>
-struct is_pointer
-{
-    static constexpr bool value = false; // return values as static constexpr
-};
+// template <typename T>
+// struct is_pointer
+// {
+//     static constexpr bool value = false; // return values as static constexpr
+// };
+
+// template <typename T>
+// struct is_pointer<T *>
+// {
+//     static constexpr bool value = true;
+// };
+
+// template <typename T>
+// inline constexpr bool is_pointer_v = is_pointer<T>::value;
+
+// class template with static member is just syntax sugar for a variadic template
+
+// is_pointer (variadic template)
 
 template <typename T>
-struct is_pointer<T *>
-{
-    static constexpr bool value = true;
-};
+constexpr bool is_pointer = false;
 
 template <typename T>
-inline constexpr bool is_pointer_v = is_pointer<T>::value;
+constexpr bool is_pointer<T *> = true;
 
 // strip_pointer
 
@@ -41,30 +51,24 @@ template <typename T>
 void
 print1(T t)
 {
-    std::cerr << __PRETTY_FUNCTION__ << '\n';
-
-    if constexpr (is_pointer_v<T> && std::is_floating_point_v<strip_pointer_t<T>>)
+    if constexpr (is_pointer<T> && std::is_floating_point_v<strip_pointer_t<T>>) // *T is floating point
     {
-        std::cout << *t;
+        std::cout << *t << ", ";
     }
     else
     {
-        std::cout << t;
+        std::cout << t << ", ";
     }
+    puts(__PRETTY_FUNCTION__);
 }
 
 template <typename A, typename B, typename C>
 void
-print3(A a, B b, C c)
+print2(A a, B b, C c)
 {
     print1(a);
-    std::cout << ", ";
-
     print1(b);
-    std::cout << ", ";
-
     print1(c);
-    std::cout << '\n';
 }
 
 int
@@ -76,8 +80,8 @@ main()
     float  a     = 10;
     double b     = 2;
 
-    print3(1, 2, 3);         // 1, 2, 3
-    print3(hello, 2, hello); // hello world, 2, hello world
-    print3(&hello, 2, 3);    // 0x7ffe558ff200, 2, 3
-    print3(&hello, &a, &b);  // 0x7ffe558ff200, 10, 2
+    print2(1, 2, 3);         // 1, 2, 3
+    print2(hello, 2, hello); // hello world, 2, hello world
+    print2(&hello, 2, 3);    // 0x7ffe558ff200, 2, 3
+    print2(&hello, &a, &b);  // 0x7ffe558ff200, 10, 2
 }
