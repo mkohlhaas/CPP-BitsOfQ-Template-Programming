16d5395479335099a495dc9fa3ea11671ef50f7f
Hans-J. Schmid <knock@myopendoor.de>
2025-07-19 04:10:52 +0200

    E08 Okays

--- Metaprogramming/E08_tuple_cat/Tuple.h
+++ Metaprogramming/E08_tuple_cat/Tuple.h
@@ -26,16 +26,20 @@ namespace bits_of_q
     };
 
     // deduction guide to make template argument deduction for constructors work (C++17)
-    // deduction guide: mapping from function (in this case constructor) -> type
+    // deduction guide: mapping from function (in this case constructor) -> type ✓
     template <typename... Ts>
-    Tuple(Ts...) -> Tuple<std::unwrap_ref_decay_t<Ts>...>;
+    Tuple(Ts...) -> Tuple<std::unwrap_ref_decay_t<Ts>...>; // remove const and refs from type
+
+    static_assert(std::is_same_v<std::unwrap_ref_decay_t<int>, int>);
+    static_assert(std::is_same_v<std::unwrap_ref_decay_t<const int>, int>);
+    static_assert(std::is_same_v<std::unwrap_ref_decay_t<const int &>, int>);
 
     // helper: make_tuple ✓
     template <typename... ELEMS>
     auto
     make_tuple(ELEMS &&...elems)
     {
-        return Tuple<std::unwrap_ref_decay_t<ELEMS>...>{std::forward<ELEMS>(elems)...};
+        return Tuple{std::forward<ELEMS>(elems)...};
     }
 
     // get
@@ -85,7 +89,7 @@ namespace bits_of_q
         };
     } // namespace detail
 
-    // size of tuple
+    // size of tuple ✓
 
     template <typename TUPLE>
     struct tuple_size;
@@ -98,8 +102,8 @@ namespace bits_of_q
     template <typename TUPLE>
     static constexpr size_t tuple_size_v = tuple_size<TUPLE>::value;
 
-    // forward as tuple (create a tuple from forwarding refs)
-
+    // forward as tuple (create a tuple from forwarding refs) ✓
+    // make_tuple removes all refs and consts, forward_as_tuple doesn't
     template <typename... Ts>
     auto
     forward_as_tuple(Ts &&...ts) noexcept
@@ -109,7 +113,7 @@ namespace bits_of_q
 
     namespace detail
     {
-        // make_tuple
+        // make_tuple_from_fwd_tuple ✓
 
         template <typename INDEX_SEQ>
         struct make_tuple_from_fwd_tuple;
@@ -122,15 +126,17 @@ namespace bits_of_q
             static auto
             f(FWD_TUPLE &&fwd)
             {
+                // `get` is from API
                 return Tuple{get<indices>(std::forward<FWD_TUPLE>(fwd))...};
             }
         };
 
-        // concat
+        // concat_with_fwd_tuple ✓
 
         template <typename FWD_INDEX_SEQ, typename TUPLE_INDEX_SEQ>
         struct concat_with_fwd_tuple;
 
+        // fwd = forward, nrm = normal
         template <size_t... fwd_indices, size_t... nrm_indices>
         struct concat_with_fwd_tuple<std::index_sequence<fwd_indices...>, std::index_sequence<nrm_indices...>>
         {
@@ -143,7 +149,7 @@ namespace bits_of_q
             }
         };
 
-        // tuple cat
+        // tuple_cat_impl ✓
 
         struct tuple_cat_impl
         {
@@ -151,13 +157,21 @@ namespace bits_of_q
             static auto
             f(FWD_TUPLE &&fwd, NRM_TUPLE &&t, TUPLES &&...ts)
             {
-                return f(concat_with_fwd_tuple<
-                             std::make_index_sequence<tuple_size_v<std::remove_cvref_t<FWD_TUPLE>>>,
-                             std::make_index_sequence<tuple_size_v<std::remove_cvref_t<NRM_TUPLE>>>>::f(std::forward<FWD_TUPLE>(fwd),
-                                                                                                        std::forward<NRM_TUPLE>(t)),
-                         std::forward<TUPLES>(ts)...);
+                return f(
+                    concat_with_fwd_tuple<
+                        // NOTE: Why is remove_cvref_t needed ? tuple_size_v doesn't work with const, ref tuple types (see video at 6:25)
+                        std::make_index_sequence<tuple_size_v<std::remove_cvref_t<FWD_TUPLE>>>,
+                        std::make_index_sequence<tuple_size_v<std::remove_cvref_t<NRM_TUPLE>>>>::f(std::forward<FWD_TUPLE>(fwd),
+                                                                                                   std::forward<NRM_TUPLE>(t)),
+                    std::forward<TUPLES>(ts)...);
             }
 
+            static_assert(std::is_same_v<std::remove_cvref_t<int>, int>);
+            static_assert(std::is_same_v<std::remove_cvref_t<int &>, int>);
+            static_assert(std::is_same_v<std::remove_cvref_t<int &&>, int>);
+            static_assert(std::is_same_v<std::remove_cvref_t<const int &>, int>);
+
+            // function overload for recursion base case
             template <typename FWD_TUPLE>
             static auto
             f(FWD_TUPLE &&ret)
